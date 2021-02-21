cd0d3d42d313704dc95c2381d7ff3aabaee25bd3
Astros257 <48536928+Astros257@users.noreply.github.com>
2021-02-21 05:11:18 -0600

    added some comments to app.jsx for the first part of the keep app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+//since our app.jsx is in the same folder as the header, footer and note we do not need to add
+//the components extension in the file when importing them.
 
 function App() {
   return (
